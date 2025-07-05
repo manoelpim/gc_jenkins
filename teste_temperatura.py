c6dcdf5b10bdf85009e54a54d8cc27c133db18db
manoelpim <205345882+manoelpim@users.noreply.github.com>
2025-07-04 22:54:59 -0400

    erro no teste

--- teste_temperatura.py
+++ teste_temperatura.py
@@ -6,7 +6,7 @@ class TestConversaoTemperatura(unittest.TestCase):
     # Teste para verificar a conversão de Fahrenheit para Celsius
     def test_fahrenheit_para_celsius(self):
         # Testando a conversão de 32°F para 0°C
-        self.assertEqual(fahrenheit_para_celsius(32), 0.0)
+        self.assertEqual(fahrenheit_para_celsius(32), 100.0)
         # Testando a conversão de 212°F para 100°C
         self.assertEqual(fahrenheit_para_celsius(212), 100.0)
 
